5b579ab761dc3d6502c5277ee3c510521ddbaa9e
Lisandro Rossi <lisandrorossi444@gmail.com>
2026-05-02 00:26:25 -0300

    Initial commit

---
+++ src/webparts/pimientaNegraPedidos/components/IPimientaNegraPedidosProps.ts
@@ -0,0 +1,7 @@
+export interface IPimientaNegraPedidosProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
